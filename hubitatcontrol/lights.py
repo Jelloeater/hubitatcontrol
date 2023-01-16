f900a96045dd9d028020f32c5fb0440301987a84
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-15 21:52:11 -0500

    Docs update

--- hubitatcontrol/lights.py
+++ hubitatcontrol/lights.py
@@ -3,11 +3,12 @@ from hubitatcontrol.hub import Device
 
 class Bulb(Device):
     @property
-    def switch(self):
+    def switch(self) -> str:
+        """Returns either (on or off)"""
         return [x for x in self.attributes if "switch" in x["name"]][0]['currentValue']
 
     @property
-    def level(self):
+    def level(self) -> int:
         return [x for x in self.attributes if "level" in x["name"]][0]['currentValue']
 
     def turn_on(self):
@@ -19,21 +20,21 @@ class Bulb(Device):
 
 class Advanced_Zigbee_RGBW_Bulb(Bulb):
     @property
-    def color_mode(self):
+    def color_mode(self) -> str:
         return [x for x in self.attributes if "colorMode" in x["name"]][0]['currentValue']
 
     @property
-    def color_name(self):
+    def color_name(self) -> str:
         return [x for x in self.attributes if "colorName" in x["name"]][0]['currentValue']
 
     @property
-    def color(self):
+    def color(self) -> str:
         return [x for x in self.attributes if "color" in x["name"]][0]['currentValue']
 
     @property
-    def color_temp(self):
+    def color_temp(self) -> int:
         return [x for x in self.attributes if "colorTemperature" in x["name"]][0]['currentValue']
 
     @property
-    def hue(self):
+    def hue(self) -> int:
         return [x for x in self.attributes if "hue" in x["name"]][0]['currentValue']
