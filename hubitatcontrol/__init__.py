4bc7dfcbd345fb681a05147dbae3d0e1abce61d9
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-15 19:25:08 -0500

    Device lookup method

--- hubitatcontrol/__init__.py
+++ hubitatcontrol/__init__.py
@@ -1,2 +1,12 @@
 """Hubitat Maker API"""
 __version__ = "0.1.1"
+
+from hubitatcontrol.lights import *
+
+
+# TODO Add hub get function
+
+def lookup_device(hub, device_lookup):
+    d = hub.get_device(device_lookup)
+    if d['type'] == 'Advanced Zigbee RGBW Bulb':
+        return Advanced_Zigbee_RGBW_Bulb(device_from_hub=d, hub=hub)
