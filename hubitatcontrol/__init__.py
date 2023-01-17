819c56a9a9aa0f9c35b3f14e4d7cb32e7344c4f0
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-17 01:36:54 -0500

    Added dimmer get method

--- hubitatcontrol/__init__.py
+++ hubitatcontrol/__init__.py
@@ -4,6 +4,7 @@ from hubitatcontrol.hub import Hub
 from hubitatcontrol.lights import *
 from hubitatcontrol.generic import *
 
+
 def get_hub(host, token, app_id) -> Hub:
     return Hub(host=host, token=token, app_id=app_id)
 
@@ -14,3 +15,5 @@ def lookup_device(hub_in, device_lookup):
         return Advanced_Zigbee_RGBW_Bulb(device_from_hub=d, hub=hub_in)
     if d['type'] == 'Generic Zigbee Outlet':
         return ZigbeeOutlet(device_from_hub=d, hub=hub_in)
+    if d['type'] == 'Leviton DZ6HD Z-Wave Dimmer':
+        return Bulb(device_from_hub=d, hub=hub_in)
