33567e8994c5cdc84aca54011918a314795ff7c7
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-19 00:27:18 -0500

    Linting

--- hubitatcontrol/generic.py
+++ hubitatcontrol/generic.py
@@ -5,18 +5,17 @@ class Switch(Device):
     @property
     def switch(self) -> str:
         """Returns either (on or off)"""
-        return [x for x in self.attributes if "switch" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "switch" in x["name"]][0]["currentValue"]
 
     def turn_on(self):
-        self.send_device_command(command='on')
+        self.send_device_command(command="on")
 
     def turn_off(self):
-        self.send_device_command(command='off')
+        self.send_device_command(command="off")
 
 
 class ZigbeeOutlet(Switch):
-
     @property
     def power(self) -> int:
         """Returns power usage"""
-        return [x for x in self.attributes if "power" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "power" in x["name"]][0]["currentValue"]
--- hubitatcontrol/lights.py
+++ hubitatcontrol/lights.py
@@ -4,32 +4,32 @@ from hubitatcontrol.generic import Switch
 class Bulb(Switch):
     @property
     def level(self) -> int:
-        return [x for x in self.attributes if "level" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "level" in x["name"]][0]["currentValue"]
 
     def set_level(self, level: int):
-        self.send_device_command(command='setLevel', secondary_command=str(level))
+        self.send_device_command(command="setLevel", secondary_command=str(level))
 
     def flash(self):
-        self.send_device_command(command='flash')
+        self.send_device_command(command="flash")
 
 
 class Advanced_Zigbee_RGBW_Bulb(Bulb):
     @property
     def color_mode(self) -> str:
-        return [x for x in self.attributes if "colorMode" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "colorMode" in x["name"]][0]["currentValue"]
 
     @property
     def color_name(self) -> str:
-        return [x for x in self.attributes if "colorName" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "colorName" in x["name"]][0]["currentValue"]
 
     @property
     def color(self) -> str:
-        return [x for x in self.attributes if "color" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "color" in x["name"]][0]["currentValue"]
 
     @property
     def color_temp(self) -> int:
-        return [x for x in self.attributes if "colorTemperature" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "colorTemperature" in x["name"]][0]["currentValue"]
 
     @property
     def hue(self) -> int:
-        return [x for x in self.attributes if "hue" in x["name"]][0]['currentValue']
+        return [x for x in self.attributes if "hue" in x["name"]][0]["currentValue"]
