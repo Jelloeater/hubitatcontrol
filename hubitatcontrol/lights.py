45eb3aa05c6c6856782082d83ac56736c8183db2
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-19 23:26:42 -0500

    fix: Unit test and taskfile

--- hubitatcontrol/lights.py
+++ hubitatcontrol/lights.py
@@ -10,7 +10,7 @@ class Bulb(Switch):
 
     def set_level(self, level: int):
         self.send_device_command(command="setLevel", secondary_command=str(level))
-        sleep(1)  # Wait for bulb to actually change to level
+        sleep(2)  # Wait for bulb to actually change to level
 
     def flash(self):
         self.send_device_command(command="flash")
