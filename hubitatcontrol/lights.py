3772ec13ad146b94f13cd54312a6ac8cf4642c45
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-24 00:49:37 -0500

    Refactored device creation to be based on capability

--- hubitatcontrol/lights.py
+++ hubitatcontrol/lights.py
@@ -12,11 +12,22 @@ class Bulb(Switch):
         self.send_device_command(command="setLevel", secondary_command=str(level))
         sleep(2)  # Wait for bulb to actually change to level
 
-    def flash(self):
-        self.send_device_command(command="flash")
+
+class ColorTempBulb(Bulb):
+    @property
+    def color_temp(self) -> int:
+        return [x for x in self.attributes if "colorTemperature" in x["name"]][0]["currentValue"]
 
 
-class Advanced_Zigbee_RGBW_Bulb(Bulb):
+class BulbRGBW(ColorTempBulb):
+    @property
+    def color(self) -> str:
+        return [x for x in self.attributes if "color" in x["name"]][0]["currentValue"]
+
+    @property
+    def hue(self) -> int:
+        return [x for x in self.attributes if "hue" in x["name"]][0]["currentValue"]
+
     @property
     def color_mode(self) -> str:
         return [x for x in self.attributes if "colorMode" in x["name"]][0]["currentValue"]
@@ -24,15 +35,3 @@ class Advanced_Zigbee_RGBW_Bulb(Bulb):
     @property
     def color_name(self) -> str:
         return [x for x in self.attributes if "colorName" in x["name"]][0]["currentValue"]
-
-    @property
-    def color(self) -> str:
-        return [x for x in self.attributes if "color" in x["name"]][0]["currentValue"]
-
-    @property
-    def color_temp(self) -> int:
-        return [x for x in self.attributes if "colorTemperature" in x["name"]][0]["currentValue"]
-
-    @property
-    def hue(self) -> int:
-        return [x for x in self.attributes if "hue" in x["name"]][0]["currentValue"]
