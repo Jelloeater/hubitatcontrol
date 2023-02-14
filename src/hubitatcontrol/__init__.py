7fd221990e3edcfbe15c90f35c6f7d153b3a24d7
Jesse Schoepfer <jelloeater@gmail.com>
2023-02-13 23:45:29 -0500

    Added flake8 and refactored init method

--- src/hubitatcontrol/__init__.py
+++ src/hubitatcontrol/__init__.py
@@ -1,8 +1,8 @@
 """Hubitat Maker API"""
 
-from hubitatcontrol.generic import *
+import hubitatcontrol.generic
+import hubitatcontrol.lights
 from hubitatcontrol.hub import Hub
-from hubitatcontrol.lights import *
 
 
 def get_hub(host, token, app_id, cloud_token=None) -> Hub:
@@ -14,15 +14,15 @@ def lookup_device(hub_in, device_lookup):
     if d is None:
         raise Exception("Device Not Found")
     if "ColorControl" in d["capabilities"] and "ColorMode" in d["capabilities"]:
-        return RGBWBulb(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.lights.RGBWBulb(device_from_hub=d, hub=hub_in)
     if "ColorTemperature" in d["capabilities"]:
-        return ColorTempBulb(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.lights.ColorTempBulb(device_from_hub=d, hub=hub_in)
     if "ChangeLevel" in d["capabilities"]:
-        return Bulb(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.lights.Bulb(device_from_hub=d, hub=hub_in)
     if "SwitchLevel" in d["capabilities"]:
-        return Dimmer(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.lights.Dimmer(device_from_hub=d, hub=hub_in)
     if "PowerMeter" in d["capabilities"] and "Outlet" in d["capabilities"]:
-        return ZigbeeOutlet(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.generic.ZigbeeOutlet(device_from_hub=d, hub=hub_in)
     if "Switch" in d["capabilities"]:
-        return Switch(device_from_hub=d, hub=hub_in)
+        return hubitatcontrol.generic.Switch(device_from_hub=d, hub=hub_in)
     return d  # Fall through return
