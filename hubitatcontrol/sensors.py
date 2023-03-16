3414ce6a34c7c29e3476db83127c0290e04a2924
Jesse Schoepfer <jelloeater@gmail.com>
2023-03-16 01:39:38 -0400

    Added temp sensor

--- hubitatcontrol/sensors.py
+++ hubitatcontrol/sensors.py
@@ -4,3 +4,10 @@ from hubitatcontrol.hub import Device
 class ContactSensor(Device):
     # TODO Add contact sensor properties
     pass
+
+
+class TemperatureSensor(Device):
+    @property
+    def temperature(self) -> int:
+        """Returns either (on or off)"""
+        return [x for x in self.attributes if "temperature" in x["name"]][0]["currentValue"]
