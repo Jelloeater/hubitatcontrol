30e412fad9c3bc8824f26d07a89d7be72bb7f6e3
Jesse Schoepfer <jelloeater@gmail.com>
2023-03-16 02:00:17 -0400

    Added timeout

--- hubitatcontrol/hub.py
+++ hubitatcontrol/hub.py
@@ -2,6 +2,8 @@ import time
 
 import requests
 
+timeout = 10
+
 
 class Hub:
     def __init__(self, host, token, app_id, cloud_token=None):
@@ -16,7 +18,7 @@ class Hub:
 
     @property
     def devices(self) -> list:
-        r = requests.get(url=self.base_url_prefix + "/all", params={"access_token": self.token}, timeout=10)
+        r = requests.get(url=self.base_url_prefix + "/all", params={"access_token": self.token}, timeout=timeout)
         return r.json()
 
     def get_device(self, name: str):
@@ -37,36 +39,47 @@ class Device:
     @property
     def commands(self):
         r = requests.get(
-            url=self.base_url_prefix + "/" + str(self.id) + "/commands", params={"access_token": self.token}
+            url=self.base_url_prefix + "/" + str(self.id) + "/commands",
+            params={"access_token": self.token},
+            timeout=timeout,
         )
         return r.json()
 
     @property
     def capabilities(self):
         r = requests.get(
-            url=self.base_url_prefix + "/" + str(self.id) + "/capabilities", params={"access_token": self.token}
+            url=self.base_url_prefix + "/" + str(self.id) + "/capabilities",
+            params={"access_token": self.token},
+            timeout=timeout,
         )
         return r.json()[0]
 
     @property
     def history(self) -> requests.Response:
-        r = requests.get(url=self.base_url_prefix + "/" + str(self.id) + "/events", params={"access_token": self.token})
+        r = requests.get(
+            url=self.base_url_prefix + "/" + str(self.id) + "/events",
+            params={"access_token": self.token},
+            timeout=timeout,
+        )
         return r.json()
 
     @property
     def attributes(self) -> requests.Response:
-        r = requests.get(url=self.base_url_prefix + "/" + str(self.id), params={"access_token": self.token})
+        r = requests.get(url=self.base_url_prefix + "/" + str(self.id), params={"access_token": self.token}, timeout=10)
         return r.json()["attributes"]
 
     def send_device_command(self, command: str, secondary_command: str = None) -> requests.Response:
         if secondary_command is None:
             r = requests.get(
-                url=self.base_url_prefix + "/" + str(self.id) + "/" + command, params={"access_token": self.token}
+                url=self.base_url_prefix + "/" + str(self.id) + "/" + command,
+                params={"access_token": self.token},
+                timeout=timeout,
             )
         else:
             r = requests.get(
                 url=self.base_url_prefix + "/" + str(self.id) + "/" + command + "/" + secondary_command,
                 params={"access_token": self.token},
+                timeout=timeout,
             )
         time.sleep(0.5)
         return r.json()
