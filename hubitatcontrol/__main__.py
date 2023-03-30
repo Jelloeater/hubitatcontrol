533986d71174ef7ec211db9ff660f85d05ee4c73
Jesse Schoepfer <jelloeater@gmail.com>
2023-03-29 22:35:03 -0400

    wip: refactored CLI args to two seperate commands

--- hubitatcontrol/__main__.py
+++ hubitatcontrol/__main__.py
@@ -1,22 +1,33 @@
 import typer
 from hubitatcontrol import get_hub
 
+app = typer.Typer(no_args_is_help=True)
 
-def print_device_list_types():
+
+@app.command()
+def print_devices_env():
     import os
-
     from dotenv import load_dotenv
 
-    envs = load_dotenv()
-    if envs:
-        host_env = os.getenv("HUBITAT_HOST")
-        token_env = os.getenv("HUBITAT_API_TOKEN")
-        app_id_env = os.getenv("HUBITAT_API_APP_ID")
-        h = get_hub(host=host_env, token=token_env, app_id=app_id_env)
-    else:
-        h = hub_creds()
-    for i in h.devices:
-        d = h.get_device(i)
+    load_dotenv()
+
+    host_env = os.getenv("HUBITAT_HOST")
+    token_env = os.getenv("HUBITAT_API_TOKEN")
+    app_id_env = os.getenv("HUBITAT_API_APP_ID")
+    cloud_token_env = os.getenv("HUBITAT_CLOUD_TOKEN")
+    h = get_hub(host=host_env, token=token_env, app_id=app_id_env, cloud_token=cloud_token_env)
+    print_device_list_types(h)
+
+
+@app.command()
+def print_devices_cli(host_env, token_env, app_id_env: int, cloud_token_env=typer.Argument(None)):
+    h = get_hub(host=host_env, token=token_env, app_id=app_id_env, cloud_token=cloud_token_env)
+    print_device_list_types(h)
+
+
+def print_device_list_types(hub_in):
+    for i in hub_in.devices:
+        d = hub_in.get_device(i)
         # TODO Need to finish device type output
 
 
@@ -25,4 +36,4 @@ def hub_creds(host_env, token_env, app_id_env):
 
 
 if __name__ == "__main__":
-    typer.run(print_device_list_types)
+    app()
