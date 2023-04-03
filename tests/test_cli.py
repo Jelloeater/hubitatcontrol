34e9f507d708deab432ec63bf746a29448a0057e
Jesse Schoepfer <jelloeater@gmail.com>
2023-04-02 23:58:06 -0400

    fix: Resolved issue with cli unit tests when package is not installed

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from dotenv import load_dotenv
+from hubitatcontrol import __main__ as cli
 
 load_dotenv()
 host_env = os.getenv("HUBITAT_HOST")
@@ -12,15 +13,11 @@ cloud_token = os.getenv("HUBITAT_CLOUD_TOKEN")
 # CLI Tests
 @pytest.mark.skipif(load_dotenv() is False, reason='No env file found')
 def test_cli_keyring():
-    import hubitatcontrol.__main__ as cli
-
     cli.load_env_to_keyring()
     cli.ls()
 
 
 @pytest.mark.skipif(load_dotenv() is False, reason='No env file found')
 def test_cli_on():
-    import hubitatcontrol.__main__ as cli
-
     cli.load_env_to_keyring()
     cli.on(257)
