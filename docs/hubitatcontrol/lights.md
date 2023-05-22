718cf4a97cead4b47ae0489ff6603e4f0c52ed46
Jesse Schoepfer <jelloeater@gmail.com>
2023-05-22 02:16:04 -0400

    wip:Started on class refactor

--- docs/hubitatcontrol/lights.md
+++ docs/hubitatcontrol/lights.md
@@ -1,122 +1,114 @@
-# Module hubitatcontrol.lights
+Module hubitatcontrol.lights
+============================
 
-## Classes
+Classes
+-------
 
 `Bulb(hub: hubitatcontrol.hub.Hub, device_from_hub: dict)`
-:
+:   
 
-```
-### Ancestors (in MRO)
+    ### Ancestors (in MRO)
 
-* hubitatcontrol.lights.Dimmer
-* hubitatcontrol.generic.Switch
-* hubitatcontrol.hub.Device
+    * hubitatcontrol.lights.Dimmer
+    * hubitatcontrol.generic.Switch
+    * hubitatcontrol.hub.Device
 
-### Descendants
+    ### Descendants
 
-* hubitatcontrol.lights.ColorTempBulb
-* hubitatcontrol.lights.ZWavePlusSceneSwitch
-```
+    * hubitatcontrol.lights.ColorTempBulb
+    * hubitatcontrol.lights.ZWavePlusSceneSwitch
 
 `ColorTempBulb(hub: hubitatcontrol.hub.Hub, device_from_hub: dict)`
-:
+:   
 
-```
-### Ancestors (in MRO)
+    ### Ancestors (in MRO)
 
-* hubitatcontrol.lights.Bulb
-* hubitatcontrol.lights.Dimmer
-* hubitatcontrol.generic.Switch
-* hubitatcontrol.hub.Device
+    * hubitatcontrol.lights.Bulb
+    * hubitatcontrol.lights.Dimmer
+    * hubitatcontrol.generic.Switch
+    * hubitatcontrol.hub.Device
 
-### Descendants
+    ### Descendants
 
-* hubitatcontrol.lights.RGBWBulb
+    * hubitatcontrol.lights.RGBWBulb
 
-### Instance variables
+    ### Instance variables
 
-`color_temp: int`
-:
+    `color_temp: int`
+    :
 
-### Methods
+    ### Methods
 
-`set_color_temp(self, level: int)`
-:   Degrees Kelvin
-```
+    `set_color_temp(self, level: int)`
+    :   Degrees Kelvin
 
 `Dimmer(hub: hubitatcontrol.hub.Hub, device_from_hub: dict)`
-:
+:   
 
-```
-### Ancestors (in MRO)
+    ### Ancestors (in MRO)
 
-* hubitatcontrol.generic.Switch
-* hubitatcontrol.hub.Device
+    * hubitatcontrol.generic.Switch
+    * hubitatcontrol.hub.Device
 
-### Descendants
+    ### Descendants
 
-* hubitatcontrol.lights.Bulb
+    * hubitatcontrol.lights.Bulb
 
-### Instance variables
+    ### Instance variables
 
-`level: int`
-:
+    `level: int`
+    :
 
-### Methods
+    ### Methods
 
-`set_level(self, level: int)`
-:   0-100 valid range
-```
+    `set_level(self, level: int)`
+    :   0-100 valid range
 
 `RGBWBulb(hub: hubitatcontrol.hub.Hub, device_from_hub: dict)`
-:
+:   
 
-```
-### Ancestors (in MRO)
+    ### Ancestors (in MRO)
 
-* hubitatcontrol.lights.ColorTempBulb
-* hubitatcontrol.lights.Bulb
-* hubitatcontrol.lights.Dimmer
-* hubitatcontrol.generic.Switch
-* hubitatcontrol.hub.Device
+    * hubitatcontrol.lights.ColorTempBulb
+    * hubitatcontrol.lights.Bulb
+    * hubitatcontrol.lights.Dimmer
+    * hubitatcontrol.generic.Switch
+    * hubitatcontrol.hub.Device
 
-### Instance variables
+    ### Instance variables
 
-`color: str`
-:
+    `color: str`
+    :
 
-`color_mode: str`
-:
+    `color_mode: str`
+    :
 
-`color_name: str`
-:
+    `color_name: str`
+    :
 
-`hue: int`
-:
+    `hue: int`
+    :
 
-`saturation: str`
-:
+    `saturation: str`
+    :
 
-### Methods
+    ### Methods
 
-`set_color(self, hue: int, saturation: int, level: int)`
-:
+    `set_color(self, hue: int, saturation: int, level: int)`
+    :
 
-`set_hue(self, hue: int)`
-:   0-100 valid range
+    `set_hue(self, hue: int)`
+    :   0-100 valid range
 
-`set_saturation(self, saturation: int)`
-:   0-100 valid range
-```
+    `set_saturation(self, saturation: int)`
+    :   0-100 valid range
 
 `ZWavePlusSceneSwitch(hub: hubitatcontrol.hub.Hub, device_from_hub: dict)`
-:
+:   
 
-```
-### Ancestors (in MRO)
+    ### Ancestors (in MRO)
 
-* hubitatcontrol.lights.Bulb
-* hubitatcontrol.lights.Dimmer
-* hubitatcontrol.generic.Switch
-* hubitatcontrol.hub.Device
-```
+    * hubitatcontrol.lights.Bulb
+    * hubitatcontrol.lights.Dimmer
+    * hubitatcontrol.generic.Switch
+    * hubitatcontrol.hub.Device
--- hubitatcontrol/__init__.py
+++ hubitatcontrol/__init__.py
@@ -6,67 +6,37 @@ import hubitatcontrol.sensors
 from hubitatcontrol.hub import Hub
 
 
-def get_device_type(device_in: hubitatcontrol.hub.Device, hub_in: hubitatcontrol.hub):
-    if device_in is None:
-        raise Exception("Device Not Found")
-    if "ColorControl" in device_in["capabilities"] and "ColorMode" in device_in["capabilities"]:
-        return hubitatcontrol.lights.RGBWBulb(device_from_hub=device_in, hub=hub_in)
-    if "ColorTemperature" in device_in["capabilities"]:
-        return hubitatcontrol.lights.ColorTempBulb(device_from_hub=device_in, hub=hub_in)
-    if "ChangeLevel" in device_in["capabilities"]:
-        return hubitatcontrol.lights.Bulb(device_from_hub=device_in, hub=hub_in)
-    if "SwitchLevel" in device_in["capabilities"]:
-        return hubitatcontrol.lights.Dimmer(device_from_hub=device_in, hub=hub_in)
-    if "PowerMeter" in device_in["capabilities"] and "Outlet" in device_in["capabilities"]:
-        return hubitatcontrol.generic.ZigbeeOutlet(device_from_hub=device_in, hub=hub_in)
-    if "Switch" in device_in["capabilities"]:
-        return hubitatcontrol.generic.Switch(device_from_hub=device_in, hub=hub_in)
-    if (
-        "TemperatureMeasurement" in device_in["capabilities"]
-        and "RelativeHumidityMeasurement" in device_in["capabilities"]
-    ):
-        return hubitatcontrol.sensors.EnvironmentalSensor(device_from_hub=device_in, hub=hub_in)
-    if "TemperatureMeasurement" in device_in["capabilities"]:
-        return hubitatcontrol.sensors.TemperatureSensor(device_from_hub=device_in, hub=hub_in)
+class GetDevices:
+    def __init__(self, hub_in: hubitatcontrol.Hub):
+        self.hub = hub_in
 
+    def __get_devices_from_capabilities__(self, capabilities_list: [str]):
+        device_list = []
+        for i in self.hub.devices:
+            if capabilities_list == i["capabilities"]:
+                device_list.append(i)
+        return device_list
 
-def get_hub(host, token, app_id, cloud_token=None) -> Hub:
-    return Hub(host=host, token=token, app_id=app_id, cloud_token=cloud_token)
+    def TemperatureSensor(self) -> list[hubitatcontrol.sensors.TemperatureSensor]:
+        return self.__get_devices_from_capabilities__(["TemperatureMeasurement"])
 
+    def EnvironmentalSensor(self) -> list[hubitatcontrol.sensors.EnvironmentalSensor]:
+        return self.__get_devices_from_capabilities__(["RelativeHumidityMeasurement", "TemperatureMeasurement"])
 
-def lookup_device(hub_in, device_lookup):
-    """
-    Takes device NAME, not ID for lookup
-    """
+    def Switch(self) -> list[hubitatcontrol.generic.Switch]:
+        return self.__get_devices_from_capabilities__(["Switch"])
 
-    return get_device_type(
-        device_in=hub_in.get_device(device_lookup), hub_in=hub_in
-    )  # Fall through return # pragma: no cover
+    def Outlet(self) -> list[hubitatcontrol.generic.ZigbeeOutlet]:
+        return self.__get_devices_from_capabilities__(["PowerMeter", "Outlet"])
 
+    def Dimmer(self) -> list[hubitatcontrol.lights.Dimmer]:
+        return self.__get_devices_from_capabilities__(["SwitchLevel"])
 
-def lookup_device_id(hub_in, device_id):
-    """
-    Takes device ID for lookup
-    """
-    return get_device_type(
-        device_in=hub_in.get_device_id(device_id), hub_in=hub_in
-    )  # Fall through return # pragma: no cover
+    def Bulb(self) -> list[hubitatcontrol.lights.Bulb]:
+        return self.__get_devices_from_capabilities__(["ChangeLevel"])
 
+    def ColorTempBulb(self) -> list[hubitatcontrol.lights.ColorTempBulb]:
+        return self.__get_devices_from_capabilities__(["ColorTemperature"])
 
-# TODO Refactor this to be dry
-def get_all_temperature_sensors(hub_in: hubitatcontrol.hub) -> list[hubitatcontrol.sensors.TemperatureSensor]:
-    """Returns list of all hub devices with associated helper functions"""
-    device_list = []
-    for i in hub_in.devices:
-        if "TemperatureMeasurement" in i["capabilities"]:
-            device_list.append(get_device_type(i, hub_in))
-    return device_list
-
-
-def get_all_environmental_sensors(hub_in: hubitatcontrol.hub) -> list[hubitatcontrol.sensors.EnvironmentalSensor]:
-    """Returns list of all hub devices with associated helper functions"""
-    device_list = []
-    for i in hub_in.devices:
-        if ("RelativeHumidityMeasurement" in i["capabilities"]) and ("TemperatureMeasurement" in i["capabilities"]):
-            device_list.append(get_device_type(i, hub_in))
-    return device_list
+    def RGBWBulb(self) -> list[hubitatcontrol.lights.RGBWBulb]:
+        return self.__get_devices_from_capabilities__(["ColorControl", "ColorMode"])
